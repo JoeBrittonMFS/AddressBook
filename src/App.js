a0741d2e5817183ddd20de5aa139fa5c4e7344a8
Joe Britton <joebritton@marlowefireandsecurity.com>
2021-01-15 11:22:00 +0000

    first commit. Set up input field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    inputText: "",
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+      <form>
+        <input type="text" value={this.state.inputText}/>
+      </form>
+
+      <div className="address-list">
+
+      </div>
+      
+
+      </div>
+    )
+  }
+
+}
+
+const Address = (props) => {
+    return 
 }
 
 export default App;
