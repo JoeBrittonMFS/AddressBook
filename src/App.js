9e2089585c2c92d1dbc593fb4b0257a109c916ee
MelindaKleszken <melindakleszken1021@gmail.com>
2021-01-15 13:42:43 +0000

    handles added to send input to addressList

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import React from 'react';
 class App extends React.Component {
   state = {
     inputText: "",
-  }
+    addressList: [],
+  };
 
+  handleInput = (event) => {
+    // getting the value of the input and assigning to the state
+    this.setState({ inputText: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    // stop default form behaviour which is to reload the page.
+    // dont want that with react spa!!!!
+    event.preventDefault();
+
+    this.setState({
+      addressList: [(this.state.addressList).push(this.state.inputText)],
+      inputText: "",
+    });
+  };
 
   render() {
     return (
       <div className="container">
-      <form>
-        <input type="text" value={this.state.inputText}/>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.inputText} onChange={this.handleInput}/>
       </form>
 
       <div className="address-list">
-
+        <h1>{this.state.inputText}</h1>
       </div>
       
 
@@ -25,8 +41,10 @@ class App extends React.Component {
 
 }
 
-const Address = (props) => {
-    return 
+/*const Address = (props) => {
+    return (
+      <p>{props.number}</p>
+    )
 }
-
+*/
 export default App;
